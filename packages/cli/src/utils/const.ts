3eb434b26273dd1299fea264ba335b259ee0b851
Aayush Rathore <aayu.r.2003@gmail.com>
2025-07-17 14:58:54 +0530

    🩹 variables issue fixed and v0.0.7 is working

--- packages/cli/src/utils/const.ts
+++ packages/cli/src/utils/const.ts
@@ -1,3 +1,3 @@
 export const DORA_VARIABLES_URL =
-    'https://raw.githubusercontent.com/doremon-me/dora-styles/main/packages/library/styles/variables.scss';
+    'https://raw.githubusercontent.com/doremon-me/dora-styles/main/packages/library/styles/scss/variables.scss';
 export const DORA_STYLES_URL = 'https://raw.githubusercontent.com/doremon-me/dora-styles/main/packages/library/styles';
