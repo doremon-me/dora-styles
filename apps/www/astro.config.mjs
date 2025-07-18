7aebeea14b3a93b19330e0a7d810d9db49400a66
Aayush Rathore <aayu.r.2003@gmail.com>
2025-07-18 16:03:37 +0530

    ✅ Home page hero section completed.

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,5 +1,44 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@styles": resolve(__dirname, "../../packages/library/styles"),
+      },
+    },
+  },
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
